23fba32e5b051280df0234d1422415e196681ed0
Fahad-Ahmed2004 <smartfahad550@gmail.com>
2025-03-04 00:23:50 +0500

    update JS file

--- app.js
+++ app.js
@@ -35,8 +35,7 @@
 
 // Q.1) Declare a variable called username.
 
-// var username
-
+// var username;
 
 // Q.2) Declare a variable called myName & assign to it a string that represents your Full Name.
 
@@ -185,4 +184,96 @@
 // document.write("<h1>Rules for naming JS variables</h1> <br>" + "<p> Variables names can only contain ,"+ " " + number + "," + " " + dollar + " " + "and" + " " + selash + " " + ". For example :" + " " + example1 + "</p>" + "<p> Variables must begin with a" + " " + letters + "," + " " + dollar + " " + "or" + " " + selash + " " + "." + " "+ "For example :" + " " + example2 + "</p> <p> Variable names are case" + " " + senSitive + "</p> <p> Variable names should not be JS"+ " " + keyWord + "</p>")
 
 // CHAPTER 04 QUESTIONS END:
+
+
+// CHAPTER 1 TO 3 QUESTIONS (WORD FILE):
+
+// Chapter 1 (Alerts);
+
+// Q.1) Alert these following (individually):
+
+// I. First Name
+// II. Last Name
+// III. Email
+// IV. Phone Number 
+// V. Password
+
+// alert("Fahad");
+// alert("Ahmed");
+// alert("1133034fahadahmed@gmail.com");
+// alert('0312-3456789');
+// alert("nopassword");
+
+// Q.2) Correct this statement: alert "You're learning JavaScript!";
+
+// alert("You're learning JavaScript!");
+
+// Q.3) Code an alert statement displaying any message you like.
+
+// alert("Hey this is Fahad Ahmed and I'm learning JavaScript from Jawan Pakistan.");
+
+
+// Chapter 2 (Variables for string);
+
+// Q.1) Declare any variable in the camel Case format.
+
+// var userNAme;
+
+// Q.2) Declare a variable of your choice without defining it. Then, in a second statement, assign it a string of your choice.
+
+// var fullName;
+
+// fullName = "Fahad Ahmed";
+
+// Q.3) Declare the variable teamName and Alert your Team name.
+
+// var teamName = "Real Madrid";
+
+// alert(teamName);
+
+// Q.4) This statement has already been coded. var bestMan = "Charlie"; Assign the variable a new string.
+
+// var bestMan = "Charlie";
+
+// bestMan = "jhony";
+
+
+// Chapter 3 (Variables for numbers);
+
+// Q.1) Declare a variable “caseQty”;
+
+var caseQty;
+
+// Q.2) Assign to the variable caseQty, which has already been declared, the value 144.
+
+// caseQty = 144;
+
+// Q.3) Rewrite this statement so the variable can be used in a math operation. var num = "9";
+
+// var num = "9";
+
+// Q.4) In one statement declare a variable. In a second statement assign it the sum of 2 numbers.
+
+// var sum;
+
+// sum = caseQty + num;
+
+// Q.5) What is the value of orderTotal?
+// var merchTotal = 100;
+// var shippingCharge = 10;
+// var orderTotal = merchTotal + shippingCharge; Try it yourself.
+
+// var merchTotal = 100;
+// var shippingCharge = 10;
+// var orderTotal = merchTotal + shippingCharge;
+
+// alert(orderTotal);
+
+// Q.6) In the first statement declare a variable and assign it a number. In the second statement, change the value of the variable by adding it together with a number.
+
+// var num = 4;
+
+// num = num + 6;
+
+// alert(num);
 
